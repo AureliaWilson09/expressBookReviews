c5d364fcd2113ef8004df180815c150711c27f85
AureliaWilson09 <wilson.a.aurelia@gmail.com>
2026-07-31 09:56:07 -0400

    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,28 +36,24 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', (req, res) =>{
-    
-    const ISBN = req.params.isbn;
-    const booksBasedOnIsbn = (ISBN) => {
-        return new Promise((resolve,reject) =>{
-          setTimeout(() =>{
-            const book = books.find((b) => b.isbn === ISBN);
-            if(book){
-              resolve(book);
-            }else{
-              reject(new Error("Book not found"));
-            }},1000);
-        });
-         
-    }
-    booksBasedOnIsbn(ISB).then((book) =>{
-      res.json(book);
-    }).catch((err)=>{
-      res.status(400).json({error:"Book not found"})
-    });    
-   
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn;
+
+  try {
+    const bookDetails = await new Promise((resolve, reject) => {
+      if (books[isbn]) {
+        resolve(books[isbn]);
+      } else {
+        reject(new Error("Book not found"));
+      }
+    });
+
+    return res.status(200).json(bookDetails);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
 });
+
   
 // Get book details based on author
 public_users.get('/author/:author', (req, res) => {
